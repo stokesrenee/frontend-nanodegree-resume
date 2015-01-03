70d2a7a5c2984950ae917297d4873a14b72343b9
Renee Stokes <stokesrenee@gmail.com>
2015-01-03 20:40:44 +0800

    Modify work experience section

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -9,20 +9,20 @@ $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 
 var bio = {
-	"name" : "Renee Stokes",
-	"role" : "Front End Web Developer",
-	"contactInfo" : {
-		"mobile" : "0481568843",
-		"email" : "stokesrenee@gmail.com",
-		"github" : "stokesrenee",
-		"twitter" : "@stokesrenee",
-		"location" : "Perth, Australia"
-	},
-	"welcomeMessage" : "Welcome to my resume online!",
-	"skills" : [
-		"HTML", "CSS", "JavaScript"
-		],
-	"bioPic" : "images/biopic.jpg"
+    "name" : "Renee Stokes",
+    "role" : "Front End Web Developer",
+    "contactInfo" : {
+        "mobile" : "0481568843",
+        "email" : "stokesrenee@gmail.com",
+        "github" : "stokesrenee",
+        "twitter" : "@stokesrenee",
+        "location" : "Perth, Australia"
+    },
+    "welcomeMessage" : "Welcome to my resume online!",
+    "skills" : [
+        "HTML", "CSS", "JavaScript"
+    ],
+    "bioPic" : "images/biopic.jpg"
 };
 
 var formattedMobile = HTMLmobile.replace("%data%", bio.contactInfo.mobile);
@@ -40,44 +40,92 @@ $("#header").append(formattedGithub);
 $("#header").append(formattedTwitter);
 $("#header").append(formattedLocation);
 $("#header").append(formattedWelcomeMessage);
-$("#header").append(HTMLskillsStart);
-$("#header").append(formattedSkills);
 $("#header").append(formattedBioPic);
 
+if (bio.skills.length > 0) {
+    $("#header").append(HTMLskillsStart);
+    var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+    $("#skills").append(formattedSkill);
+    formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+    $("#skills").append(formattedSkill);
+    formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+    $("#skills").append(formattedSkill);
+    }
+
 var education = {
-	"universities": [
-	{
-		"name" : "Murdoch University",
-		"location" : "Perth, Australia",
-		"degree" : "Bachelor of Arts",
-		"majors" : [
-			"Psychology", "Education"
-			],
-		"dates" : "1998",
-		"url" : "http://www.murdoch.edu.au"
-	},
-	{
-		"name" : "Curtin University",
-		"location" : "Perth, Australia",
-		"degree" : "Graduate Diploma",
-		"majors" : "Information Science",
-		"dates" : "2012",
-		"url" : "http://www.curtin.edu.au"
-	}
-	],
-	"onlineCourses": [
-	{
-		"title" : "Front End Developer NanoDegree",
-		"courseProvider" : "Udacity",
-		"dates" : 2015,
-		"url" : "http://www.udacity.com"
-	}
-	]
+    "schools": [
+        {
+            "name" : "Murdoch University",
+            "location" : "Perth, Australia",
+            "degree" : "Bachelor of Arts",
+            "majors" : [
+                "Psychology", "Education"
+            ],
+            "dates" : "1998",
+            "url" : "http://www.murdoch.edu.au"
+        },
+        {
+            "name" : "Curtin University",
+            "location" : "Perth, Australia",
+            "degree" : "Graduate Diploma",
+            "majors" : "Information Science",
+            "dates" : "2012",
+            "url" : "http://www.curtin.edu.au"
+        }
+    ],
+    "onlineCourses": [
+        {
+            "title" : "Front End Developer NanoDegree",
+            "courseProvider" : "Udacity",
+            "dates" : 2015,
+            "url" : "http://www.udacity.com"
+        },
+    ],
 };
 
+var formattedName = HTMLschoolName.replace("%data%", education.schools.name);
+var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools.location);
+var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools.degree);
+var formattedMajors = HTMLschoolMajor.replace("%data%", education.schools.majors);
+var formattedDates = HTMLschoolDates.replace("%data%", education.schools.dates);
+
+$("#education").append(HTMLschoolStart);
+$("#education").append(formattedName);
+$("#education").append(formattedLocation);
+$("#education").append(formattedDegree);
+$("#education").append(formattedMajors);
+$("#education").append(formattedDates);
+
 var work = {
-	"employer" : "Endeavour College",
-	"title" : "Campus Librarian",
-	"location" : "Perth, Australia",
-	"dates" : "2012 - 2015"
+    "jobs" : [
+        {
+        "employer" : "Endeavour College",
+        "title" : "Campus Librarian",
+        "location" : "Perth, Australia",
+        "dates" : "2012 - 2015",
+        "description" : "I am currently managing the Perth branch of the Endeavour College Library"
+        },
+    ],
 };
+
+function displayWork () {
+    for (job in work.jobs) {
+        $("#workExperience").append(HTMLworkStart);
+
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+        var formattedEmployerTitle = formattedEmployer + formattedTitle;
+        $(".work-entry:last").append(formattedEmployerTitle);
+
+        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+        $(".work-entry:last").append(formattedDates);
+
+        var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+        $(".work-entry:last").append(formattedLocation);
+
+        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+        $(".work-entry:last").append(formattedDescription);
+        }
+}
+
+displayWork ();
