d832f61e5c8afe55b566924e4ed1832e591a44d4
Renee Stokes <stokesrenee@gmail.com>
2015-01-04 20:48:28 +0800

    Add extra jobs and education

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -55,12 +55,27 @@ if (bio.skills.length > 0) {
 var education = {
     "school": [
         {
+            "name" : "Inspire Education",
+            "location" : "Perth, Australia",
+            "degree" : "Certificate IV in Training and Assessment",
+            "dates" : "2014",
+            "url" : "http://www.inspireeducation.net.au"
+        },
+        {
             "name" : "Curtin University",
             "location" : "Perth, Australia",
             "degree" : "Graduate Diploma",
             "majors" : "Information Science",
             "dates" : "2011-2012",
             "url" : "http://www.curtin.edu.au"
+        },
+        {
+            "name" : "Wellington College of Education",
+            "location" : "Wellington, New Zealand",
+            "degree" : "Graduate Diploma",
+            "majors" : "Teaching (Primary)",
+            "dates" : "2003",
+            "url" : "http://www.victoria.ac.nz/education"
         },
         {
             "name" : "Murdoch University",
@@ -76,9 +91,33 @@ var education = {
     "onlineCourses": [
         {
             "title" : "Front End Developer NanoDegree",
-            "courseProvider" : "Udacity",
+            "school" : "Udacity",
             "dates" : 2015,
             "url" : "http://www.udacity.com"
+        },
+        {
+            "title": "Intro to Computer Science",
+            "school": "Udacity",
+            "dates": "2015",
+            "url": "https://www.udacity.com/course/cs101"
+        },
+        {
+            "title": "Intro to HTML and CSS",
+            "school": "Udacity",
+            "dates": "2015",
+            "url": "https://www.udacity.com/course/ud304"
+        },
+        {
+            "title": "Version Control with Git and GitHub",
+            "school": "Udacity",
+            "dates": "2015",
+            "url": "https://www.udacity.com/course/ud775"
+        },
+        {
+            "title": "JavaScript Basics",
+            "school": "Udacity",
+            "dates": "2015",
+            "url": "https://www.udacity.com/course/ud804"
         }
     ]
 };
@@ -103,17 +142,82 @@ $(".education-entry:last").append(formattedMajors);
 
 displayEducation ();
 
+function displayOnlineCourses () {
+	for (course in education.onlineCourses) {
+
+var formattedSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[course].school);
+var formattedTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[course].title);
+var formattedURL = HTMLonlineURL.replace("%data%", education.onlineCourses[course].url);
+var formattedDates = HTMLonlineDates.replace("%data%", education.onlineCourses[course].dates);
+
+$(".education-entry:last").append(formattedTitle + formattedSchool);
+$(".education-entry:last").append(formattedDates);
+$(".education-entry:last").append(formattedURL);
+$(".education-entry:last").append("<br><div><hr class='section-hr'></div>");
+    }
+}
+
+	displayOnlineCourses ();
+
 var work = {
     "jobs" : [
         {
-        "employer" : "Endeavour College",
-        "title" : "Campus Librarian",
-        "location" : "Perth, Australia",
-        "dates" : "2012 - 2015",
-        "description" : "I am currently managing the Perth branch of the Endeavour College Library."
+            "employer" : "Endeavour College",
+            "title" : "Campus Librarian",
+            "location" : "Perth, Australia",
+            "dates" : "2012 - 2015",
+            "description" : "I am currently managing the Perth branch of the Endeavour College Library."
+        },
+        {
+            "employer" : "Westbooks Library Services",
+            "title" : "Cataloguer",
+            "location" : "Perth, Australia",
+            "dates" : "2011 - 2015",
+            "description" : "Creating catalogue records for resources acquired by the State Library of Western Australia."
+        },
+        {
+            "employer" : "Suzuka Language School",
+            "title" : "English Teacher",
+            "location" : "Suzuka, Japan",
+            "dates" : "2008 - 2011",
+            "description" : "Teaching Japanese students of all ages to speak, read and write English."
+        },
+        {
+            "employer" : "Perth International Business College",
+            "title" : "English Teacher",
+            "location" : "Perth, Australia",
+            "dates" : "2007 - 2008",
+            "description" : "Teaching international students of all ages to speak, read and write English so that they can pass an exam to enter an Australian university."
+        },
+        {
+            "employer" : "Theresiunum Catholic College",
+            "title" : "English Teacher",
+            "location" : "Eisenstadt, Austria",
+            "dates" : "2005 - 2007",
+            "description" : "Teaching Austrian highschool students to speak, read and write English so that they can pass an exam to enter university."
+        },
+        {
+            "employer" : "Wellington Council",
+            "title" : "Swimming Teacher",
+            "location" : "Wellington, New Zealand",
+            "dates" : "2003",
+            "description" : "During my studies, I coached children in the skills of swimming."
         }
     ]
 };
+
+function locationizer(work_obj) {
+	var locationArray = [];
+
+	for (job in work_obj.jobs) {
+		var newLocation = work_obj.jobs[job].location;
+		locationArray.push(newLocation);
+
+    }
+	return locationArray;
+}
+
+console.log(locationizer(work));
 
 function displayWork () {
     for (job in work.jobs) {
@@ -136,3 +240,15 @@ function displayWork () {
 }
 
 displayWork ();
+
+
+$("#main").append(internationalizeButton);
+var inName = function(_name) {
+    var splitName = _name.split(" ");
+    var firstName = splitName[0][0].toUpperCase() + splitName[0].slice(1);
+    var lastName = splitName[1].toUpperCase();
+    var internationalName = firstName + " " + lastName;
+    return internationalName;
+};
+
+
